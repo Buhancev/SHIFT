58fcb3c321bf6a889826521a93452c50ad157a3d
Buhancev <maxim.buhancev@gmail.com>
2022-07-10 19:56:03 +0700

    Initial commit

---
+++ src/main/java/com/bbuhha/alivetostipa/AliveToStipaApplication.java
@@ -0,0 +1,12 @@
+package com.bbuhha.alivetostipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliveToStipaApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(AliveToStipaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bbuhha/alivetostipa/AliveToStipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbuhha.alivetostipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliveToStipaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
